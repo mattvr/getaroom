96c64d4265713b065cd600c33d4eda58433b468e
Ransom Roberson <ranmantwo@gmail.com>
2015-03-26 14:11:23 -0400

    Started working on parsing to database (Blitz)

--- getaroom.py
+++ getaroom.py
@@ -4,89 +4,124 @@ import datetime
 import time
 import collections
 import sys
-import getopt
+import re
+from blitzdb import Document
+from blitzdb import FileBackend
+
+# Example match:
+# M W F
+# 12:20PM
+# 1:10PM
+# LITRV 1670
+pattern = ("((?:(?:M|T|W|R|F)\s?)+)\s*\n"
+           "(?P<start_time>\d\d?:\d\d(?:AM|PM))\n"
+           "(?P<end_time>\d:\d\d(?:AM|PM))\n"
+           "(?:(?P<building>(?:\w)+)) (?P<room>(?:\w| )*)")
+
+regex = re.compile(pattern)
 
 day_lookup = {
-"M": 0,
-"T": 1,
-"W": 2,
-"R": 3,
-"F": 4
+    "M": 0,
+    "T": 1,
+    "W": 2,
+    "R": 3,
+    "F": 4
 }
 
 # eg. 1:25PM
 IN_TIME_FORMAT = "%i:%M%p"
 
+# database
+backend = FileBackend("data")
+
 # Supports:
-# help
-# in
+#   help
+#   in
 #	import
 #	export
 #	populate
 def main(argv):
     directive = "help"
-    try:
-        opts, args = getopt.getopt(argv, "hb:i:x:p:", ["help",
-                                                       "building=",
-                                                       "import=",
-                                                       "export=",
-                                                       "populate="])
-    except getopt.GetoptError:
-        print("Improper invocation")
-        sys.exit(2);
+    options = {"help"     : pub_help,
+               "in"       : pub_get_room_in,
+               "import"   : pub_import_json,
+               "export"   : pub_export_json,
+               "populate" : pub_populate}
+    if options.has_key(argv[0]):
+        directive = argv[0]
+    options[directive](argv[1:])
 
 
-def populate():
+def pub_help(args):
+    print("Help")
+    return
+def pub_get_room_in(args):
+    print("Getting a room in "+ args[0])
+    return
+def pub_import_json(args):
+    return
+def pub_export_json(args):
+    return
+def pub_populate(args):
+    if len(args) == 0:
+        source_file = 'timetable.html'
+    else:
+        source_file = args[0]
+
+    populate(source_file)
+    return
+
+def populate(source_file):
+
+    print "Loading %s..." % source_file,
+    soup = BeautifulSoup(open(source_file, 'r'))
+    print '\033[92m'+"done"+'\033[0m'
+
+    rows = soup.table.tbody.find_all('tr')
+    rows = rows[1:]  # ignore title row
+
     for i, row in enumerate(rows):
-        cells = row.find_all('td')
+        text = row.get_text().encode('ascii', 'ignore')
 
-        # Get the classroom
-        first = cells[-1].get_text().encode('ascii', 'ignore')
-        loc_str = cells[-2].get_text().encode('ascii', 'ignore')
-        start_str = cells[-4].get_text().encode('ascii', 'ignore')
-        end_str = cells[-3].get_text().encode('ascii', 'ignore')
-
-        # Ignore TBA rooms
-        if (loc_str == 'TBA' or start_str == '(ARR)' or first == 'Staff'):
+        # See if pattern matches
+        result = regex.match(text)
+        if (result is None):
             continue
 
-        loc_split = loc_str.split(' ', 1)
-        building = loc_split[0]
+        # Get capture groups
+        days = result.group("days")
+        building = result.group("building")
+        room = result.group("room")
+        start_time = result.group("start_time")
+        end_time = result.group("end_time")
 
-        number = ''
-        if len(loc_split) > 1:
-            number = loc_split[1]
 
-        # Add new building
-        if building not in buildings:
-            buildings[building] = []
+        print "%s %s" % (building, room)
+        
+        #question: we have 2 building objects v ^
+        #how do we distinguish oops
+        db_building = backend.get(Building, {'id': building})
 
-        room = Classroom()
-        room.number = number
+        time = Time({'start': start_time, 'end': end_time, 'days': days})
+        schedule = Schedule({'period': 'Fall 2015', 'time': time}) # WONT need to merge
 
-        # Add new rooms
-        if room not in buildings[building]:
-            new_room = Classroom()
-            new_room.number = number
-            buildings[building].append(new_room)
 
-        # Parse days & times
-        start_str = cells[-4].get_text().encode('ascii', 'ignore')
+        if db_building is None:
+            db_building = Building({'id': building, 'rooms': [room]})
+        else:
+            db_rooms = db_building.rooms
 
-        # time_start = time.strptime(start_str, IN_TIME_FORMAT)
-        # time_end   = time.strptime(end_str, IN_TIME_FORMAT)
+            if db_rooms is None:
+                db_rooms = [Room({'id': room})]
 
-        days_str = cells[-5].get_text().encode('ascii', 'ignore')
+            db_room = db_rooms.get(Room, {'id': room})
 
-        print days_str
+            if db_room is None:
+                db_room = Room({'id': room})
 
-        # Convert M -> 0, T -> 1, etc.
-        days_split = days_str.split(' ')
-        days_converted = []
-    # for day in days_split:
-    # 	print day
-    # days_converted.append(day_lookup[day])
-    # buildings[building].days = days_converted
+            db_building.rooms.append(room)
+
+        db_building.save(backend)
 
 
 def get_available_rooms(building, time):
@@ -98,7 +133,7 @@ def get_available_rooms(building, time):
     if len(avail) > 0:
         print 'There are %d available rooms in your building.' % len(avail)
         for room in avail:
-            print room.number
+            print room.id
         # print room.days
     else:
         # Idea: Log most commonly requested buildings following this request. Offer room in that
@@ -116,16 +151,22 @@ class Classroom(object):
     # def when_available():
 
 
-soup = BeautifulSoup(open('timetable.html', 'r'))
+# Database Docs
+class Building(Document):
+    pass
+class Room(Document):
+    pass
+class Schedule:
+    pass
+class Time:
+    pass
 
-rows = soup.table.tbody.find_all('tr')
-rows = rows[1:]  # ignore title row
-
-buildings = {}  # Name, List of Classes
-
-# populate()
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    if len(sys.argv) > 1:
+        main(sys.argv[1:])
+    else:
+        pub_help(0)
 
-get_available_rooms(buildings['MCB'], 1500)
+
+buildings = {}  # Name, List of Classes
