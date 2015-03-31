a90c4ec452ec3c4a960a419ca1e81bfd66a8f416
Ransom Roberson <ranmantwo@gmail.com>
2015-03-31 15:24:34 -0400

    * Added room weights * Cleaned up code

--- getaroom.py
+++ getaroom.py
@@ -1,9 +1,12 @@
 from datetime import datetime
+import operator
 import time
 import sys
 import re
 import sqlite3
 import json
+
+from config import SQLITE_DATABASE, BUIlDING_NAME_LOOKUP
 
 # External dependencies
 from bs4 import BeautifulSoup
@@ -14,17 +17,19 @@ from bs4 import BeautifulSoup
 # 1:10PM
 # # LITRV 1670
 pattern = "(?P<days>(?:(?:M|T|W|R|F)\s?)+)\s*(?P<start_time>\d\d?:\d\d(?:AM|PM))\s*(?P<end_time>\d\d?:\d\d(?:AM|PM))\s*(?:(?P<building>(?:\w)+)) (?P<room>(?:\w| )*)"
-database = "timetable.db"
 
 regex = re.compile(pattern)
 
 day_lookup = {
-    "M": 0,
-    "T": 1,
-    "W": 2,
-    "R": 3,
-    "F": 4
+    1: "M",
+    2: "T",
+    3: "W",
+    4: "R",
+    5: "F",
+    6: "X",
+    7: "S"
 }
+
 
 day_translation = {
     "Sunday"    : "S",
@@ -43,6 +48,11 @@ TIME_OUT_FORMAT = "%H:%M"
 
 class ClassRoom:
     def __init__(self):
+        self.building_code = ""
+        self.building_name = ""
+        self.number = ""
+        self.weight = 0
+        self.end_availability = False
         pass
 
 # Supports:
@@ -62,79 +72,25 @@ def main(argv):
 
 
 def pub_help(args):
-    print("Usage: ")
+    print("Usage: python getaroom.py")
+    print("                           in [room]")
+    print("                           pupulate [source]")
     return
 
 
 def pub_get_room_in(args):
     building_string = args[0]
     print("Getting a room in " + building_string + "...")
-    con = sqlite3.connect('data.db')
-
-    # building_string = args[0]
-    # First ensure we have that building
-    cur = con.cursor()
-    cur.execute("SELECT * FROM buildings WHERE code = '" + building_string + "';")
-    rows = cur.fetchall()
-    if len(rows) == 0:
-        print "Sorry, we don't have that building in our database! Try another."
-    building = rows[0]
-
-    # Now let's get all the rooms from the building
-    cur.execute("SELECT * FROM rooms WHERE building_id = '" + str(building[0]) + "';")
-    rooms = cur.fetchall()
+    rooms = get_available_rooms(building_string, datetime.now())
     if len(rooms) == 0:
-        print "No rooms found in that building. How strange."
+        print "Sorry, no rooms are available in that building right now! :("
         return
-    print "Found %i rooms!" % len(rooms)
+    for room in rooms:
+        if not room.end_availability:
+            print "%s %s is available for the rest of the day" % (room.building_code, room.number)
+        else:
+            print "%s %s is available until %s" % (room.building_code, room.number, room.end_availability)
 
-    valid_rooms = []
-    for db_room in rooms:
-        cur.execute("SELECT * FROM times WHERE room_id = '"+ str(db_room[0]) +"';")
-        times = cur.fetchall()
-
-        is_valid = True
-        next_unavailable = None
-
-        for class_time in times:
-            class_days = json.loads(class_time[5])
-            current_day = day_translation[time.strftime("%A")]
-
-            # Only look at classes occuring today
-            if current_day not in class_days:
-                continue
-
-            time_start = time.strptime(class_time[3], TIME_OUT_FORMAT)
-            time_end = time.strptime(class_time[4], TIME_OUT_FORMAT)
-
-            if time_contained(time_start, time_end, datetime.now()):
-                is_valid = False
-                break
-
-            # Next class should represent when room is occupied
-
-            # Next_unavailable is unset, and class_time > this time
-            if next_unavailable is None and time_greater(time_start.tm_hour, time_start.tm_min, datetime.now().hour, datetime.now().minute):
-                next_unavailable = time_start
-            # Class_time < next_unavailable
-            elif next_unavailable is not None and time_greater(time_start.tm_hour, time_start.tm_min, datetime.now().hour, datetime.now().minute) and time_lesser(time_start.tm_hour, time_start.tm_min, next_unavailable.tm_hour, next_unavailable.tm_min):
-                next_unavailable = time_start
-
-        if is_valid:
-            valid_rooms.append((db_room, next_unavailable))
-
-    if len(valid_rooms) == 0:
-        print "Sorry, there are no rooms available in this building right now. :("
-    else:
-        # TODO: print next class time for this room
-        print "Got %i available rooms:" % len(valid_rooms)
-        for room, room_time in valid_rooms:
-            if room_time is not None:
-                time_as_str = time.strftime(TIME_IN_FORMAT, room_time)
-                print "%s %s is available until %s" % (building[1], room[1], time_as_str)
-            else:
-                print "%s %s is available for the rest of the day." % (building[1], room[1])
-    cur.close()
 
 # This is ugly, I know.
 def time_contained(start, end, time):
@@ -168,8 +124,8 @@ def pub_populate(args):
 
 # Writes a SQLite database from source_file html table
 def populate(source_file):
-    con = sqlite3.connect('data.db')
-    building_lookup = json.loads(open("buildings.json").read())
+    con = sqlite3.connect(SQLITE_DATABASE)
+    building_lookup = json.loads(open(BUIlDING_NAME_LOOKUP).read())
 
 
     print "Loading %s..." % source_file,
@@ -240,8 +196,83 @@ def populate(source_file):
     con.close()
 
 
-def get_available_rooms(building, time):
-    pass
+def get_available_rooms(building_str, in_class_time):
+    con = sqlite3.connect(SQLITE_DATABASE)
+    cur = con.cursor()
+    cur.execute("SELECT * FROM buildings WHERE code = '" + building_str + "';")
+    rows = cur.fetchall()
+    if len(rows) == 0:
+        # TODO: Log building not found
+        return []
+    building = rows[0]
+
+    # Now let's get all the rooms from the building
+    cur.execute("SELECT * FROM rooms WHERE building_id = '" + str(building[0]) + "';")
+    rooms = cur.fetchall()
+    if len(rooms) == 0:
+        # TODO: Log no rooms found
+        return []
+
+    valid_rooms = []
+    for db_room in rooms:
+        cur.execute("SELECT * FROM times WHERE room_id = '"+ str(db_room[0]) +"';")
+        times = cur.fetchall()
+
+        is_valid = True
+        next_unavailable = None
+
+        for class_time in times:
+            class_days = json.loads(class_time[5])
+
+            weekday = in_class_time.isoweekday()
+            current_day = day_lookup[weekday]
+
+            # Only look at classes occuring today
+            if current_day not in class_days:
+                continue
+
+            time_start = time.strptime(class_time[3], TIME_OUT_FORMAT)
+            time_end = time.strptime(class_time[4], TIME_OUT_FORMAT)
+
+            if time_contained(time_start, time_end, datetime.now()):
+                is_valid = False
+                break
+            # Next class should represent when room is occupied
+            # Next_unavailable is unset, and class_time > this time
+            if next_unavailable is None and time_greater(time_start.tm_hour, time_start.tm_min, in_class_time.hour, in_class_time.minute):
+                next_unavailable = time_start
+            # Class_time < next_unavailable
+            elif next_unavailable is not None and time_greater(time_start.tm_hour, time_start.tm_min, in_class_time.hour, in_class_time.minute) and time_lesser(time_start.tm_hour, time_start.tm_min, next_unavailable.tm_hour, next_unavailable.tm_min):
+                next_unavailable = time_start
+
+        if is_valid:
+            valid_rooms.append((db_room, next_unavailable))
+
+    # No timeslots available
+    if len(valid_rooms) == 0:
+        return []
+    else:
+        return_rooms = []
+        for room, room_time in valid_rooms:
+            result_class_room = ClassRoom()
+            result_class_room.building_code = building[1]
+            result_class_room.building_name = building[2]
+            result_class_room.number = room[1]
+            result_class_room.weight = 1
+            if room_time is not None:
+                time_as_str = time.strftime(TIME_IN_FORMAT, room_time)
+                result_class_room.end_availability = time_as_str
+                # print "%s %s is available until %s" % (building[1], room[1], time_as_str)
+            else:
+                result_class_room.end_availability = False
+                result_class_room.weight += 20
+                # print "%s %s is available for the rest of the day." % (building[1], room[1])
+            return_rooms.append(result_class_room)
+    cur.close()
+
+    return_rooms.sort(key=operator.attrgetter('weight'), reverse=True)
+    return return_rooms
+
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
--- server.py
+++ server.py
@@ -1,42 +1,76 @@
+import json
+from datetime import datetime
+import httplib, urllib
+
 # External dependencies
-import wit
-from flask import Flask
+import Queue
+from flask import Flask, request
 from nexmomessage import NexmoMessage # pip install -e git+https://github.com/marcuz/libpynexmo.git#egg=nexmomessage
-from config import WIT_ACCESS_TOKEN
+
+from config import WIT_ACCESS_TOKEN, NEXMO_API_KEY, NEXMO_API_SECRET, NEXMO_PHONE_NO
+from getaroom import get_available_rooms, ClassRoom
 
 app = Flask(__name__)
 
-@app.route('/getaroom')
+@app.route('/getaroom', methods=['GET', 'POST'])
 def getaroom():
-    # sender_no = request.args['msisdn']
-    # body = request.args['text']
+    sender_no = request.values.get('msisdn')
+    body = request.values.get('text')
+
+    if sender_no is None or body is None:
+        return 'Invalid message.'
 
     # TODO: account for concatenated & unicode messages
-    #
-    # wit.init()
-    # wit.text_query("get a room in torg", WIT_ACCESS_TOKEN, wit_callback)
+    wit_response = send_to_wit(body)
+    sms_response = parse_response(json.loads(wit_response))
+    send_sms(sender_no, sms_response)
+    print wit_response
 
-    # must return 200 response!
-    return 'Success!'
-    
+    return sms_response
 
-def wit_callback(response):
-	print('Response: {}'.format(response))
-	return 'lol'
-	wit.close()
-	
-# msg = {
-# 	'reqtype': 'json',
-# 	'api_key': NEXMO_API_KEY,
-# 	'api_secret': NEXMO_API_SECRET,
-# 	'from': NEXMO_PHONE_NO,
-# 	'to': sender_no,
-# 	'text': 'Your message has been received.'
-# }
-# sms = NexmoMessage(msg)
-# sms.set_text_info(msg['text'])
-# sms.send_request()
+def send_to_wit(message):
+    conn = httplib.HTTPSConnection('api.wit.ai')
+    headers = {'Authorization': 'Bearer %s' % (WIT_ACCESS_TOKEN)}
+    params = urllib.urlencode({'v': '20141022', 'q': message})
+    url = '/message?%s' % (params)
+    conn.request('GET', url, '', headers)
+    response = conn.getresponse()
+    return response.read()
 
+def parse_response(response):
+    if 'outcomes' in response and len(response['outcomes']) > 0:
+        outcome = response['outcomes'][0]
+        if 'entities' in outcome and len(outcome['entities']) > 0:
+            entity = outcome['entities']
+            if 'building' in entity and len(entity['building']) > 0:
+                building = entity['building'][0]['value']
+
+                rooms = get_available_rooms(building, datetime.now())
+                if len(rooms) == 0:
+                    return "Sorry, there aren't any rooms available in that building right now."
+                else:
+                    best_room = rooms[0]
+                    if not best_room.end_availability:
+                        return "%s %s is available for the rest of the day" % (best_room.building_code, best_room.number)
+                    else:
+                        return "%s %s is available until %s" % (best_room.building_code, best_room.number, best_room.end_availability)
+
+    return "Invalid message. Try 'get a room in TORG'"
+
+def send_sms(number, message):
+    msg = {
+        'reqtype'   : 'json',
+        'api_key'   : NEXMO_API_KEY,
+        'api_secret': NEXMO_API_SECRET,
+        'from'      : NEXMO_PHONE_NO,
+        'to'        : number,
+        'text'      : message
+    }
+    sms = NexmoMessage(msg)
+    sms.set_text_info(msg['text'])
+    response = sms.send_request()
+    if not response:
+        print "Failed to send response"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', debug=True)
