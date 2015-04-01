a7d18ff0fc7afb45c364203177725c9ec1196b3d
Ransom Roberson <ranmantwo@gmail.com>
2015-03-31 21:56:14 -0400

    Added example config

---
+++ config.example.py
@@ -0,0 +1,13 @@
+NEXMO_API_KEY = ''
+NEXMO_API_SECRET = ''
+NEXMO_PHONE_NO = ''
+
+WIT_ACCESS_TOKEN = ''
+
+SQLITE_DATABASE = 'data/data.db'
+BUIlDING_NAME_LOOKUP = 'data/buildings.json'
+
+LOGGER_SERVER = 'logs/server.log'
+LOGGER_SERVICE = 'logs/getaroom.log'
+
+BLACKLIST = 'config/blacklist.json'
