d42f07b048852e9f47466ead7124a629f1c8136a
Ransom Roberson <ranmantwo@gmail.com>
2015-04-02 15:08:17 -0400

    Updated database for rate limiting Matt cleaned up the server some Gonna try some different rate limiting stuff

--- rate_limit_service.py
+++ rate_limit_service.py
@@ -1,12 +1,40 @@
 from datetime import datetime
+import time
 import logging as logger
 import sqlite3
 
 from config import SMS_PER_PERIOD, SMS_PERIOD, SQLITE_DATABASE
 
+DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S'
 
 def update_rate(phone_number):
-    pass
+    con = sqlite3.connect(SQLITE_DATABASE)
+    cur = con.cursor()
+
+    q = "SELECT * FROM rate_limit_logs WHERE phone_number = ?"
+    cur.execute(q, (phone_number,))
+
+    rate = cur.fetchone()
+    # current_time = datetime.strftime(DATETIME_FORMAT)
+    current_time = time.strftime(DATETIME_FORMAT)
+    if rate is None:
+        logger.info("Creating new rate limiting entry for %s" % phone_number)
+        q = "INSERT INTO rate_limit_logs VALUES (NULL, ?, ?, NULL, ?);"
+        cur.execute(q, (phone_number, current_time, 0))
+    else:
+        datet1 = datetime.strptime(rate[2], DATETIME_FORMAT)
+
+        datet2 = datetime.strptime(rate[3], DATETIME_FORMAT)
+
+        new_count = rate[4] + 1
+        new_time = current_time
+        if (datet1 - datet2).seconds > SMS_PERIOD:
+            new_count = 0
+
+        q = "UPDATE rate_limit_logs SET count = ?, last_time = ? WHERE phone_number = ?"
+        cur.execute(q, (new_count, current_time, phone_number))
+    cur.close()
+
 
 def is_rate_limited(phone_number):
     con = sqlite3.connect(SQLITE_DATABASE)
@@ -24,5 +52,15 @@ def is_rate_limited(phone_number):
     t2 = rate[3]
     count = rate[4]
 
+    retval = False
     if not t2:
-        return False
+        retval = False
+    elif count > SMS_PER_PERIOD:
+        retval = True
+
+    # datet1 = datetime.strptime(t1, DATETIME_FORMAT)
+    # datet2 = datetime.strptime(t2, DATETIME_FORMAT)
+    # if (datet1 - datet2).seconds > SMS_PERIOD
+
+    cur.close()
+    return retval
--- server.py
+++ server.py
@@ -1,9 +1,10 @@
 import random
 import json
+import math
 from datetime import datetime
 import httplib, urllib
 import logging as logger
-
+from rate_limit_service import  is_rate_limited, update_rate
 # External dependencies
 from flask import Flask, request
 import sqlite3
@@ -33,13 +34,20 @@ def getaroom():
     # TODO: account for concatenated & unicode messages
     wit_response = send_to_wit(body)
     sms_response = parse_response(json.loads(wit_response))
-    if DEBUG_SMS:
-        print("SMS DEBUG:\n%s\nfrom: %s\n===========" % (sms_response, sender_no))
-    else:
-        send_sms(sender_no, sms_response)
-    print wit_response
+    ret_val = ""
 
-    return sms_response
+    update_rate(sender_no)
+    if is_rate_limited(sender_no):
+        print("Number rate limited.")
+    else:
+        if DEBUG_SMS:
+            print("SMS DEBUG:\n%s\nfrom: %s\n===========" % (sms_response, sender_no))
+        else:
+            send_sms(sender_no, sms_response)
+        print wit_response
+
+        ret_val = sms_response
+    return ret_val
 
 def send_to_wit(message):
     conn = httplib.HTTPSConnection('api.wit.ai')
@@ -69,8 +77,6 @@ def parse_getaroom(response):
             if 'building' in entity and len(entity['building']) > 0:
                 building = entity['building'][0]['value']
 
-                numTexts = 0;
-
                 rooms = get_available_rooms(building, datetime.now())
                 if len(rooms) == 0:
                     return "Sorry, there aren't any rooms available in that building right now."
@@ -93,11 +99,10 @@ def parse_getaroom(response):
                     if i is not iterations - 1:
                         string += '\n'
 
-                numTexts = 1 + (len(string) / 160) # this is the number of texts sent
+                numTexts = math.floor(1 + (len(string) / 160)) # this is the number of texts sent
                 logger.info("SMS Response Generated - consumes %d SMS" % numTexts)
                 print("SMS Response Generated - consumes %d SMS" % numTexts)
                 return string
-
     return "Invalid message. Try 'get a room in TORG'"
 
 def is_banned(number):
@@ -108,7 +113,7 @@ def is_banned(number):
 
 def parse_joke(response):
     string = 'Congratulations! You have been signed up for Emperor Penguin Facts.\n\n'
-    num = random.randint(0, 4)
+    num = random.randint(0, 3)
     if num is 0:
         string += 'Did you know emperor penguins stand up to 4ft tall?'
     elif num is 1:
@@ -116,18 +121,9 @@ def parse_joke(response):
     elif num is 2:
         string += 'Emperor penguins are featured on more than 30 countries stamps.'
     elif num is 3:
-        string += 'Emperor penguins fast longer than any other bird, going 115 days without eating during chick incubation!'
-    elif num is 4:
-        string += 'Penguins. Penguins. Pengiuns. Pegnuins. PENGUISN. PENGEUNINS. PENGSUINSG OEPJG SODFKSDFO FSDFDSFSS'
+        string += 'Emperor penguins fast longer than any other bird, going 115 days without eating.'
 
     return string
-
-def check_rate_limited(number):
-    con = sqlite3.connect(SQLITE_DATABASE)
-    with con:
-        cur = con.cursor()
-        cmd = 'SELECT * FROM users WHERE phone_no = ?'
-        cur.execute(cmd, (number,))
 
 def send_sms(number, message):
     msg = {
